9416fecab335b1e6d3cef37a346995b0909c9b50
Chris Fenos <chris.m.fenos@gmail.com>
2018-08-25 12:10:38 -0400

    Initial react navigation and apollo provider wrapped in reedux store

--- App.js
+++ App.js
@@ -1,46 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux'
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset';
+
 import store from './store'
 import AppNavigation from './src/Navigation'
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:4000' }),
+  cache: new InMemoryCache(),
+});
 
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <AppNavigation />
+        <ApolloProvider client={client}>
+          <AppNavigation />
+        </ApolloProvider>
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
